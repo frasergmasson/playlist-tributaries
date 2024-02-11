7edd0dcc60b430e2e1a75c186469ef1e9d5ba9fe
frasergmasson <frasergmasson@gmail.com>
2024-02-11 16:38:26 +0000

    create component layout

--- src/components/Combiner.js
+++ src/components/Combiner.js
@@ -1,22 +1,54 @@
-import { Box } from "grommet"
+import { Box, Button, Tag } from "grommet"
 import { useState } from "react";
 import { Select } from "grommet";
 
 function Combiner({playlists}){
 
-    const [selected, setSelected] = useState('');
+    const [selected1, setSelected1] = useState('');
+    const [selected2, setSelected2] = useState('');
+    const [selectedOutput, setselectedOutput] = useState('');
+
+    const combinePlaylists = async (e) => {
+        e.preventDefault()
+
+        if (selected1 === selectedOutput || selected2 === selectedOutput || selected1 === selected2){
+            //error
+            return
+        }
+      }
 
     return (
-        <Box fill align="center" justify="start" pad="large" gap="medium">
+        <Box fill align="center" justify="start" pad="large" gap="medium" direction="row">
             <Select
-                id="playlistSelect"
-                name="playlistSelect"
+                id="playlistSelect1"
+                name="playlistSelect1"
                 placeholder="Select"
                 options={playlists}
                 valueKey={{ key: 'id', reduce: true}}
                 labelKey="name"
-                onChange={({ value: nextValue }) => setSelected(nextValue)}
+                onChange={({ value: nextValue }) => setSelected1(nextValue)}
             />
+            <Tag value="+"/>
+            <Select
+                id="playlistSelect2"
+                name="playlistSelect2"
+                placeholder="Select"
+                options={playlists}
+                valueKey={{ key: 'id', reduce: true}}
+                labelKey="name"
+                onChange={({ value: nextValue }) => setSelected2(nextValue)}
+            />
+            <Tag value="=>"/>
+            <Select
+                id="outputPlaylist"
+                name="outputPlaylist"
+                placeholder="Select"
+                options={playlists}
+                valueKey={{ key: 'id', reduce: true}}
+                labelKey="name"
+                onChange={({ value: nextValue }) => setselectedOutput(nextValue)}
+            />
+            <Button onClick={combinePlaylists} />
         </Box>
     )
 }
